f952b3e4aee4990cbcf15df8bd0f71e52e8b399c
Samuel Schlemper <samuelschlemper2006@gmail.com>
2025-07-01 10:14:58 -0300

    Trying again to remove some files

--- rust_see_primes/target/release/deps/rust_see_primes-8369aea513b3ae00.d
+++
@@ -1,5 +0,0 @@
-/home/schlemuel/Documentos/Programacao/performance_testing/rust_see_primes/target/release/deps/rust_see_primes-8369aea513b3ae00: src/main.rs
-
-/home/schlemuel/Documentos/Programacao/performance_testing/rust_see_primes/target/release/deps/rust_see_primes-8369aea513b3ae00.d: src/main.rs
-
-src/main.rs:
